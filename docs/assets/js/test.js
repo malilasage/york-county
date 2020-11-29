1bdaa68571b76a0093a7407ab12be67b235692a7
malilasage <malilaclearwater@gmail.com>
2020-11-29 15:08:09 -0800

    restructure js

--- docs/assets/js/test.js
+++ docs/assets/js/test.js
@@ -1,28 +1,21 @@
-console.log("hello");
-window.addEventListener('load', function() {
-    // console.log('All assets are loaded');
-    removeClass();
-    showSlides(slideIndex);
-
-})
-
-
-
 $(window).on("load", () => {
-      $("#progress").addClass("progress");
-      navClickListener();
+  var initSlideIndex = 1;
+
+  removeClass();
+  showSlides(initSlideIndex);
+
+  $("#progress").addClass("progress");
+  navClickListener();
 });
 
 function navClickListener(){
-      $("nav li a").click(() => {
-        var linkId = $(event.target).attr("href");
-
-        $("nav li a").removeClass();
-        if(linkId != "#title") {
-          $(event.target).addClass("active-nav");
-        }
-
-      });
+  $("nav li a").click(() => {
+  var linkId = $(event.target).attr("href");
+    $("nav li a").removeClass();
+    if(linkId != "#title") {
+      $(event.target).addClass("active-nav");
+    }
+  });
 }
 
 function removeClass(){
@@ -35,7 +28,6 @@ function removeClass(){
     const callback = (entries, observer) => {
       entries.forEach(entry => {
     		const { target } = entry;
-    		// console.log(entry, target)
 
     		if (entry.intersectionRatio >= 0.25) {
     			target.classList.add("is-visible");
@@ -53,14 +45,11 @@ function removeClass(){
 };
 
 var slideIndex = 1;
-//showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -86,98 +75,6 @@ function showSlides(n) {
   }
 }
 
-
-
-
-  window.addEventListener('scroll', () => {
-    // console.log(window.pageYOffset);
-    //console.log(document.body.offsetHeight);
-    // console.log(window.innerHeight);
-    // console.log(document.body.scrollHeight);
-    /*
-var body = document.body,
-    html = document.documentElement;
-
-var height = Math.max( body.scrollHeight, body.offsetHeight,
-                       html.clientHeight, html.scrollHeight, html.offsetHeight );
-        or just use the jquery
-    */
-    document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.scrollHeight - window.innerHeight));
-  }, false);
-//////
-  //import Reveal from '{{ site.baseurl }}/assets/js/reveal.js/dist/reveal.esm.js';
-  //import Markdown from '{{ site.baseurl }}/assets/js/reveal.js/plugin/markdown/markdown.esm.js';
-
-//td: figure out mousewheel, embed, markdown, and start setting up and adding content (just enough for concept proof), change content layout for err msg, disable non local img links
-/*$(window).on("load", () => {
-  let deck1 = new Reveal(document.querySelector('.deck1'), {
-    //embedded: true,
-    touch: true,
-    //keyboardCondition: 'focused',
-    mouseWheel: true,
-    plugins: [ RevealMarkdown ]
-  });
-  deck1.initialize();
-})*/
-
-//deck1.initialize();
-  /*Reveal.initialize({
-    plugins: [ RevealMarkdown ]
-  });*/
-
-
-/*const sections = [...document.querySelectorAll("section")];
-
-let options = {
-  rootMargin: "0px",
-  threshold: 0.25
-};
-
-const callback = (entries, observer) => {
-  entries.forEach(entry => {
-		const { target } = entry;
-		console.log(entry, target)
-
-		if (entry.intersectionRatio >= 0.25) {
-			target.classList.add("is-visible");
-		} else {
-			target.classList.remove("is-visible");
-		}
-  });
-};
-
-const observer = new IntersectionObserver(callback, options);
-
-sections.forEach((section, index) => {
-  observer.observe(section);
-});
-
-window.onload(removeClass());*/
-
-/*(function() {
-  $(window).on("load", () => {
-    $(window).scroll(function() {
-      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-
-      $(".masthead").each(function() {
-        var objectBottom = $(this).offset().top + $(this).outerHeight();
-
-        if (objectBottom < windowBottom) {
-
-            if($(".masthead").css("display")=="none"){
-              $(".masthead").stop().animate({
-                "display": "block"
-              });
-            }
-          } else {
-            //something that's not display jfc
-            if ($(".masthead").css("display")=="block"){
-              $(".masthead").stop().animate({
-              "display": "none"
-              });
-            }
-          }
-        })
-      })
-    })
-})();*/
+window.addEventListener('scroll', () => {
+  document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.scrollHeight - window.innerHeight));
+}, false); //change height access to jquery

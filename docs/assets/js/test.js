07369b4c6a48988299d7a4bb2d8ad3f7df17d2bf
malilasage <malilaclearwater@gmail.com>
2020-11-28 18:48:15 -0800

    added context carousel

--- docs/assets/js/test.js
+++ docs/assets/js/test.js
@@ -2,6 +2,7 @@ console.log("hello");
 window.addEventListener('load', function() {
     // console.log('All assets are loaded');
     removeClass();
+    showSlides(slideIndex);
 })
 
 function removeClass(){
@@ -29,9 +30,43 @@ function removeClass(){
     slides.forEach((slide, index) => {
       observer.observe(slide);
     });
+};
+
+var slideIndex = 1;
+//showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("carousel-slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  if(slideIndex != 1) {
+    $('html, body').animate({
+      scrollTop: $("#context").offset().top
+    }, 1000);
+  }
+}
+
+//////
   //import Reveal from '{{ site.baseurl }}/assets/js/reveal.js/dist/reveal.esm.js';
   //import Markdown from '{{ site.baseurl }}/assets/js/reveal.js/plugin/markdown/markdown.esm.js';
 
